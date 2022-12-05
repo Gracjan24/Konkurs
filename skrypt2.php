31896775af6ae458ffe9c7cfe88daccddbd21d7b
Gracjan24 <gracjan.grzeszczak.kg@gmail.com>
2022-12-05 21:22:45 +0100

    skrypt 2

--- skrypt2.php
+++ skrypt2.php
@@ -14,53 +14,58 @@
         <form method="post">
             <button name="losowanie">Losuj</button>
         </form>
+        <div class="answerBlok"></div>
     </div>
     <div class="blok">
         <!-- <button onclick></button> -->
         <?php
-            // if(isset($_POST['losowanie'])) {
-            //     $conn = mysqli_connect('localhost', 'root', '', 'baza');
-            //     $zapytanie = "SELECT `pol`, `ang`, `bad`, `wrong` FROM slownictwo WHERE id = RAND()";
-            //     $result = mysqli_query($conn, $zapytanie);
-            //     if (mysqli_num_rows($result) > 0) {
-            //         while ($row = mysqli_fetch_assoc($result)) {
-            //             $_SESSION['pol'] = $row['pol'];
-            //             $_SESSION['ang'] = $row['ang'];
-            //             $_SESSION['bad'] = $row['bad'];
-            //             $_SESSION['wrong'] = $row['wrong'];
-            //         }
-            //     }
-            //     echo '<h4>Przetlumacz wyraz na jezyk angielski</h4>';
-                $w1 = $w2 = $w3 = true;
-                for ($i = 0; $i < 3; $i++) {
-                    $w = rand(1,3);
-                    if ($w == 1) {
-                        if ($w1 == true) {
-                            echo 'ang';
-                            $w1 = false;
-                        } else {
-                            $i--;
-                        }
-                    }
-                    if ($w == 2) {
-                        if ($w2 == true) {
-                            echo 'bad';
-                            $w2 = false;
-                        } else {
-                            $i--;
-                        }
-                    }
-                    if ($w == 1) {
-                        if ($w3 == true) {
-                            echo 'wrong';
-                            $w3 = false;
-                        } else {
-                            $i--;
-                        }
+            if(isset($_POST['losowanie'])) {
+                $conn = mysqli_connect('localhost', 'root', '', 'konkurs');
+                $zapytanie = "SELECT `pol`, `ang`, `bad`, `wrong` FROM slownictwo WHERE id = 1";
+                $result = mysqli_query($conn, $zapytanie);
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $_SESSION['pol'] = $row['pol'];
+                        $_SESSION['ang'] = $row['ang'];
+                        $_SESSION['bad'] = $row['bad'];
+                        $_SESSION['wrong'] = $row['wrong'];
                     }
                 }
-            }
+                echo $_SESSION['pol'];
 
+                echo '<h4>Przetlumacz wyraz na jezyk angielski</h4>';
+                $r = rand(1,6);
+                if ($r == 1) {
+                    echo '<div class="answerBlok">'.$_SESSION['ang'].'</div>';
+                    echo '<div class="answerBlok">'.$_SESSION['bad'].'</div>';
+                    echo '<div class="answerBlok">'.$_SESSION['wrong'].'</div>';
+                }
+                if ($r == 2) {
+                    echo '<div class="answerBlok">'.$_SESSION['wrong'].'</div>';
+                    echo '<div class="answerBlok">'.$_SESSION['ang'].'</div>';
+                    echo '<div class="answerBlok">'.$_SESSION['bad'].'</div>';
+                }
+                if ($r == 3) {
+                    echo '<div class="answerBlok">'.$_SESSION['bad'].'</div>';
+                    echo '<div class="answerBlok">'.$_SESSION['wrong'].'</div>';
+                    echo '<div class="answerBlok">'.$_SESSION['ang'].'</div>';
+                }
+                if ($r == 4) {
+                    echo '<div class="answerBlok">'.$_SESSION['wrong'].'</div>';
+                    echo '<div class="answerBlok">'.$_SESSION['bad'].'</div>';
+                    echo '<div class="answerBlok">'.$_SESSION['ang'].'</div>';
+                }
+                if ($r == 5) {
+                    echo '<div class="answerBlok">'.$_SESSION['ang'].'</div>';
+                    echo '<div class="answerBlok">'.$_SESSION['wrong'].'</div>';
+                    echo '<div class="answerBlok">'.$_SESSION['bad'].'</div>';
+                }
+                if ($r == 6) {
+                    echo '<div class="answerBlok">'.$_SESSION['bad'].'</div>';
+                    echo '<div class="answerBlok">'.$_SESSION['ang'].'</div>';
+                    echo '<div class="answerBlok">'.$_SESSION['wrong'].'</div>';
+                }
+            }
         ?>
     </div>
 </body>
